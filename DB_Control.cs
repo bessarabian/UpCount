767d243dcce40cd03ca02dde91cbfe40807dd79c
Gleb Yeromin <vaskou221@gmail.com>
2023-02-14 12:55:41 +0200

    migrating from .net framework to .net

--- DB_Control.cs
+++ DB_Control.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using Dapper;
-using System.Linq;
-using Microsoft.EntityFrameworkCore.Sqlite;
+﻿using System.IO;
 using Microsoft.EntityFrameworkCore;
 
 namespace UpCount
 {
     public class DB_Control : DbContext
     {
-        public DB_Control() 
-        {
-            
-        }
-
         public static string GetUpCountFolder()
         {
             string currentDirectory = Directory.GetCurrentDirectory();
@@ -28,40 +18,6 @@ namespace UpCount
             string mock = string.Format("{0}\\db\\", GetUpCountFolder());
             string[] files = Directory.GetFiles(mock, "*.sqlite", SearchOption.AllDirectories);
             return string.Format("Data Source={0}", files[0]);
-        }
-
-        public void InitDB()
-        {
-            string finalPath = string.Format("{0}\\db\\database.sqlite", GetUpCountFolder());
-            if (!File.Exists(finalPath))
-            {
-                Console.WriteLine("No database file found, creating...");
-                File.Create(finalPath);
-                Console.WriteLine("Created database by following path: " + GetConnectionString());
-                CreateUserTable();
-
-            } else { Console.WriteLine("DB Inited."); }
-            
-        }
-
-        public void CreateUserTable()
-        {
-            using S cnn = new(GetConnectionString());
-            cnn.Execute(@"
-                   CREATE TABLE IF NOT EXISTS Expenses(
-	                ""ID""	INTEGER NOT NULL UNIQUE,
-	                ""AMOUNT""	INTEGER NOT NULL,
-                    ""DATE""	TEXT NOT NULL,
-                    ""CURRENCY""	TEXT NOT NULL,
-                    ""CATEGORY""	TEXT NOT NULL,
-                    PRIMARY KEY(""ID"" AUTOINCREMENT))");
-        }
-
-        public List<Expense> LoadExpenses()
-        {
-            using SQLiteConnection cnn = new(@"Data Source=../../db/database.sqlite");
-            var output = cnn.Query<Expense>("select * from Expenses", new DynamicParameters());
-            return output.ToList();
         }
     }
 }
